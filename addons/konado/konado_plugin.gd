02cd2e92ba9c9b81a79f83b2346fff4ccdc0ea19
DSOE1024 <computerlub@163.com>
2026-05-06 16:58:30 +0800

    feat: 更新Konado至2.4.0版本并添加启动横幅

--- addons/konado/konado_plugin.gd
+++ addons/konado/konado_plugin.gd
@@ -4,7 +4,7 @@ class_name KonadoEditorPlugin
 # Konado框架入口文件，负责初始化插件和注册相关功能
 
 ## 插件版本信息
-const VERSION: String = "2.4"
+const VERSION: String = "2.4.0"
 const CODENAME: String = "Macaron"
 
 ## 自定义EditorImportPlugin脚本
